82f57a900fe23d16bb3bc987bd117cdd7279a766
Sean Baldwin <sean.rivis@gmail.com>
2017-10-06 06:14:12 -0400

    initial upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="./style/main.css">
+	</head>
+	<body>
+		<h1>Test 1 3</h1>
+		<?php 
+		echo "Hello";
+		?>
+		<script type="text/javascript" src="js/main.js"></script>
+	</body>
+</html>
